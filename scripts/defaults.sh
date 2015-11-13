9ca1c7bd918a4b5030829b9122e8c33856a5c0ca
a-morales <antonio.morales@originate.com>
2015-11-13 15:01:30 -0800

    invert some defaults

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -4,8 +4,8 @@ setDefaults() {
   tmux set-option -g message-command-style "fg=white,bg=black"
   tmux set-option -g message-style "fg=white,bg=black"
   tmux set-option -g status-style "fg=white,bg=black"
-  tmux set-option -g window-status-current-style "fg=green,bg=black"
+  tmux set-option -g window-status-current-style "fg=black,bg=green"
   tmux set-option -g window-style "fg=white,bg=black"
-  tmux set-option -g window-status-bell-style "fg=blue,bg=black"
-  tmux set-option -g window-status-activity-style "fg=red,bg=black"
+  tmux set-option -g window-status-bell-style "fg=black,bg=blue"
+  tmux set-option -g window-status-activity-style "fg=black,bg=red"
 }
